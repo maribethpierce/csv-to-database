5898d7a9ba0587a57a8079b484ab2fe902529445
Maribeth <maribethpierce@gmail.com>
2015-10-31 21:07:30 -0400

    code

--- code.rb
+++ code.rb
@@ -1,6 +1,7 @@
 #YOUR CODE GOES HERE
 require 'pg'
 require 'pry'
+require 'csv'
 
 def db_connection
   begin
@@ -15,5 +16,16 @@ db_connection do |conn|
   a = conn.exec("SELECT * FROM ingredients")
   a.each do |item|
     puts "#{item['id']}. #{item['ingredient']}"
+  end
+end
+# only add stuff once: (doesn't work right now...)
+if ingredients.empty?
+  CSV.foreach('ingredients.csv') do |row|
+    db_connection do |list_item|
+      list_item.exec_params("INSERT INTO ingredients ('id') VALUES ($1)", [row['ingredient']])
+    end
+    ensure
+      connection.close
+    end
   end
 end
